8704d1bfb68d75731e3b5d37ce2c0b9556132d79
caihualin <CaiHLwebmail@gmail.com>
2023-02-15 00:33:10 +0800

    group

---
+++ cache.go
@@ -0,0 +1,33 @@
+package geecache
+
+import (
+	"geeCache/pkg/lru"
+	"sync"
+)
+
+type cache struct {
+	mu         sync.Mutex
+	lru        *lru.Cache
+	cacheBytes int64
+}
+
+func (c *cache) add(key string, value ByteView) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	if c.lru == nil {
+		c.lru = lru.New(c.cacheBytes, nil)
+	}
+	c.lru.Add(key, value)
+}
+
+func (c *cache) get(key string) (value ByteView, ok bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	if c.lru == nil {
+		return
+	}
+	if get, ok := c.lru.Get(key); ok {
+		return get.(ByteView), ok
+	}
+	return
+}
---
+++ pkg/getter/getter.go
@@ -0,0 +1,12 @@
+package getter
+
+// load data for key
+type Getter interface {
+	Get(key string) ([]byte, error)
+}
+
+type GetterFunc func(key string) ([]byte, error)
+
+func (f GetterFunc) Get(key string) ([]byte, error) {
+	return f(key)
+}
